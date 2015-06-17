ad773930d237d9e1a494da1cab0a6a1e5c95e55c
Wenbin Wu <wenbin.wu@schibsted.es>
2015-06-17 16:11:50 +0200

    unicode for hotsite

--- todaqui/hotsite/models.py
+++ todaqui/hotsite/models.py
@@ -5,3 +5,6 @@ class HotSite(models.Model):
     name = models.CharField(max_length=64)
     link = models.URLField(max_length=512)
     img_name = models.CharField(max_length=16)
+
+    def __unicode__(self):
+        return self.name
