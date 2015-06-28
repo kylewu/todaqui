23f60ecf6d0bb7795abfd4a4c4bb09ffd563c570
Wenbin Wu <admin@wenbinwu.com>
2015-06-28 02:13:59 +0200

    custom manager

--- todaqui/common/models.py
+++ todaqui/common/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 
+class OrderedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(OrderedManager, self).get_queryset().order_by('order')
+
+    def ordered(self):
+        return self.get_queryset().order_by('order')
+
+    def available(self):
+        return self.get_queryset().filter(enable=True)
+
+
 class OrderedModel(models.Model):
     order = models.SmallIntegerField(default=1)
     enable = models.BooleanField(db_index=True, default=True)
+
+    objects = OrderedManager()
 
     class Meta:
         abstract = True
--- todaqui/index/views.py
+++ todaqui/index/views.py
@@ -5,8 +5,8 @@ from ..topic.models import Topic
 
 
 def index(request):
-    hotsites = HotSite.objects.all()
-    topics = Topic.objects.filter(enable=True)
+    hotsites = HotSite.objects.available()
+    topics = Topic.objects.available()
     return render(request, "index.html", {
         'hotsites': hotsites,
         'topics': topics
